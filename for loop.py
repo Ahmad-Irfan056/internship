ce114ba8b50fc6dba52182eeb69128ec376dc85f
Bilal <Bilal@gmail.com>
2025-05-12 20:29:31 +0500

    loops

---
+++ for loop.py
@@ -0,0 +1,19 @@
+print("fruits")
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print("I like", fruit)
+
+print("SUM of numbers")
+
+total = 0
+for i in range(1, 6):
+    total += i
+print("Sum:", total)
+
+print("nested")
+for i in range(1, 4):
+    for j in range(1, 4):
+        print(i, "x", j, "=", i*j)
+    print("------")
+
+
