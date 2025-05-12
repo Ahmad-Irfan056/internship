a2bf788be9637e42ccacdacf7b7c92b4674cdc1d
Bilal <Bilal@gmail.com>
2025-05-12 20:42:05 +0500

    while loop

---
+++ while loop.py
@@ -0,0 +1,14 @@
+print(1-5)
+i = 1
+while i <= 5:
+    print(i)
+    i += 1  
+
+print("while with if")
+while True:
+    number = int(input("Enter a number (0 to stop): "))
+    if number == 0:
+        break
+    print("You entered:", number)
+
+
