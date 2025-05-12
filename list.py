ef279e03122c14699f4fbb3d3be0d13bec70e86c
Bilal <Bilal@gmail.com>
2025-05-13 00:22:02 +0500

    list

---
+++ list.py
@@ -0,0 +1,30 @@
+
+my_list = []
+numbers = [1, 2, 3, 4, 5]
+mixed = [1, "hello", 3.14, True]
+print ("my_list",my_list,)       
+print ("numbers",numbers,)       
+print ("mixed",mixed,)    
+
+print("insert element")
+fruits = ["apple", "banana"]
+fruits.append("orange")
+print("Fruits after append:", fruits)
+
+print("insert element to specific index")
+
+numbers = [10, 20, 30]
+numbers.insert(1, 15)
+print("After inserting 15:", numbers)
+print("numbers",numbers)
+
+print("remove element")
+animals = ["cat", "dog", "rabbit"]
+animals.remove("dog")
+print("After removing dog:", animals)
+
+print("pop ")
+names = ["ibrahim", "subhan", "ali"]
+removed = names.pop()
+print("After pop:", names)
+print("Removed name:", removed)
