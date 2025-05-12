1e7a93f007406354b239ea7e4f7959cdf95a9fb9
Bilal <Bilal@gmail.com>
2025-05-13 00:02:11 +0500

    functions added

---
+++ functions.py
@@ -0,0 +1,17 @@
+
+def add_numbers(a, b):
+    return a + b
+result = add_numbers(10, 5)
+print("The sum is:", result)
+
+def add_numbers(c, d):
+    return c + d
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+answer = add_numbers(num1, num2)
+print("Sum is:", answer)
+
+def say_hello():
+    print("Hello, welcome to Python!")
+say_hello()
